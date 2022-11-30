2d0f189f182e28e0bb36a75e8f7c43ef38eae333
RainbowToad <chad_spilker@hotmail.com>
2022-11-30 12:20:38 -0800

    API routes created

--- Develop/server.js
+++ Develop/server.js
@@ -17,8 +17,28 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static(`./public`));
 
+//API GET ROUTE
+app.get(`/api/notes`, function(req, res) {
+    readFileAsync(`./db/db.json`, `utf8`).then(function(data) {
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+    })
+});
 
+// API POST request
+app.post(`/api/notes`, function(req, res) {
+    const note = req.body;
+    readFileAsync(`./db/db.json`, `utf8`).then(function(data) {
+        const notes = [].concat(JSON.parse(data));
+        note.id = notes.length + 1
+        notes.push(note);
+        return notes
+    }).then(function(notes) {
+        writeFileAsync(`./db/db.json`, JSON.stringify(notes))
+        res.json(note);
 
+    })
+});
 
 
 
