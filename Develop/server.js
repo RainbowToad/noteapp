a5cd280a99a5356edeb310fd848857e1f916ed73
RainbowToad <chad_spilker@hotmail.com>
2022-11-30 10:15:49 -0800

    PORT listening verification

--- Develop/server.js
+++ Develop/server.js
@@ -3,3 +3,48 @@ const express = require(`express`);
 const path = require(`path`);
 const fs = require(`fs`);
 const util = require(`util`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Verification of listening PORT
+app.listen(PORT, function() {
+    console.log(`App listening on PORT ` + PORT);
+});
